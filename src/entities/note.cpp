57304530fd4a01af7d2c22482c61210324e48328
Thomas Berthelot <tb.publicemail@gmail.com>
2020-08-22 07:00:42 +0200

    Fix bug of reference management with filenames containing '-' or '_'

--- src/entities/note.cpp
+++ src/entities/note.cpp
@@ -3197,7 +3197,7 @@ void Note::updateReferencedNote(QString linkedNotePath, QString currentNotePath)
 		// Next, check if links are available and create/update them
 		QString path = relativeFilePath(currentNotePath);
 
-		match = QRegularExpression(R"(\*\s\[[A-Za-zÀ-ÖØ-öø-ÿ0-9\%\s\*]*\]\()" + path.replace(" ","%20") + R"(\))").match(text);
+		match = QRegularExpression(R"(\*\s\[[A-Za-zÀ-ÖØ-öø-ÿ0-9\%\s\*\_\-]*\]\()" + path.replace(" ","%20") + R"(\))").match(text);
 		
 		// No link to current note in "Referenced by" section yet, add it
 		if (!match.hasMatch()) {
